af3685eddc595687721ebda8abf01a551e3e09c5
Ayisha Alli <alli.ayisha1@gmail.com>
2021-11-12 21:28:50 +0000

    used javascript to make underline brighter when hovered

--- app.js
+++ app.js
@@ -5,3 +5,21 @@ bars.addEventListener('click', function showMenu(){
     navList.classList.toggle('show')
     console.log('hi')
 })
+
+const secTwoBtn = document.querySelector('.secTwoBtn');
+const secThreeBtn = document.querySelector('.secThreeBtn');
+
+secThreeBtn.addEventListener('mouseover', function (){
+    document.getElementById('underlineTwo').style.opacity = '1';
+})
+secThreeBtn.addEventListener('mouseout', function (){
+    document.getElementById('underlineTwo').style.opacity = '0.4';
+})
+
+secTwoBtn.addEventListener('mouseover', function (){
+    document.getElementById('underlineOne').style.opacity = '1';
+})
+secTwoBtn.addEventListener('mouseout', function (){
+    document.getElementById('underlineOne').style.opacity = '0.4';
+})
+// Definately need to refractor this. I need to work out how to target multiple class items at one time.
